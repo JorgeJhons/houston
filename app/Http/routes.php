aff3dc7aac0f46bca4cd02bccd6cc172c7ae4382
Jorge Fernandez <jfernandezcolli@gmail.com>
2016-02-05 18:40:13 -0600

    Se agrega, el routing de la pagina

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -32,3 +32,11 @@ Route::group(['middleware' => ['web']], function () {
 	});
     
 });
+
+Route::group(['middleware' => 'web'], function () {
+	
+    Route::auth();
+
+    Route::get('/home', 'HomeController@index');
+
+});
